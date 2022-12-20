f9810d70ebb8c69e8ffb412e8d606da801573764
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-12-20 15:58:13 +0000

    building functions for inquirer

--- src/question.js
+++ src/question.js
@@ -1,11 +1,33 @@
 //main menu questions
 //inquirer takes an array of objects as its input 
+//function to run main question
 
-const StarterQuestions = [
-    {
-        name: "user_name",
-        type: "input",
-        message: "What is your name?",  
-    }
-]
+const { default: inquirer } = require("inquirer")
+
+function starterQuestions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What action would you like to take?',
+            name: 'action_choice',
+            choices: ['Add an employee', 'Add an engineer', 'Add an intern', 'Add a manager', 'Build team']
+
+        },
+
+    ])
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What is the employee\s name?',
+            name: '',
+            choices: ['Add an employee', 'Add an engineer', 'Add an intern', 'Add a manager', 'Build team']
+
+
+
+        }
+    ])
+}
 
