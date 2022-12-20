36e1c12ca201f0cb1643081ec2ebfd849ef0b9c6
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-12-20 15:42:23 +0000

    added all tests, 3 pass, manager test failed

---
+++ src/question.js
@@ -0,0 +1,11 @@
+//main menu questions
+//inquirer takes an array of objects as its input 
+
+const StarterQuestions = [
+    {
+        name: "user_name",
+        type: "input",
+        message: "What is your name?",  
+    }
+]
+
