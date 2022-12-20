b1258644c747993f2ca32446a5949ca255f33041
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-12-20 16:05:03 +0000

    finished creating functions containing questions for inquirer

--- src/question.js
+++ src/question.js
@@ -20,14 +20,128 @@ function starterQuestions() {
 function addEmployee() {
     inquirer.prompt([
         {
-            type: 'list',
+            type: 'input',
             message: 'What is the employee\s name?',
-            name: '',
-            choices: ['Add an employee', 'Add an engineer', 'Add an intern', 'Add a manager', 'Build team']
+            name: 'employee_name',
+            
+        },
 
+        {
+            type: 'input',
+            message: 'What is the employee\s ID?',
+            name: 'employee_id',
 
+        },
 
-        }
+        {
+            type: 'input',
+            message: 'What is the employee\s email?',
+            name: 'employee_email',
+
+        },
     ])
 }
+
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the engineers\s name?',
+            name: 'engineer_name',
+            
+        },
+
+        {
+            type: 'input',
+            message: 'What is the engineer\s ID?',
+            name: 'engineer_id',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the engineer\s email?',
+            name: 'engineer_email',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the engineer\s GitHub profile name?',
+            name: 'engineer_github',
+
+        },
+    ])
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the intern\s name?',
+            name: 'intern_name',
+            
+        },
+
+        {
+            type: 'input',
+            message: 'What is the engineer\s ID?',
+            name: 'intern_id',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the intern\s email?',
+            name: 'intern_email',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the intern\s school name?',
+            name: 'intern_school',
+
+        },
+    ])
+}
+
+function addManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the manager\s name?',
+            name: 'manager_name',
+            
+        },
+
+        {
+            type: 'input',
+            message: 'What is the manager\s ID?',
+            name: 'manager_id',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the manager\s email?',
+            name: 'manager_email',
+
+        },
+
+        {
+            type: 'input',
+            message: 'What is the manager\s office nubmer?',
+            name: 'manager_officeNubmer',
+
+        },
+    ])
+}
+
+
+
+
+
+
 
